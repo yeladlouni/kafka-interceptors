98f474462a0365fa834e1ee62aea2097e0382377
yassine <yeladlouni@gmail.com>
2023-07-26 16:05:01 +0100

    added main function

--- src/main/java/ExampleConsumer.java
+++ src/main/java/ExampleConsumer.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class ExampleConsumer {
+import org.apache.kafka.clients.consumer.ConsumerInterceptor;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.TopicPartition;
+
+import java.util.Map;
+
+public class ExampleConsumer implements ConsumerInterceptor<String, String> {
+    @Override
+    public ConsumerRecords<String, String> onConsume(ConsumerRecords<String, String> records) {
+        System.out.println("onConsume");
+        return records;
+    }
+
+    @Override
+    public void onCommit(Map<TopicPartition, OffsetAndMetadata> offsets) {
+        System.out.println("onCommit");
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void configure(Map<String, ?> configs) {
+
+    }
 }
--- src/main/java/ExampleProducer.java
+++ src/main/java/ExampleProducer.java
@@ -1,18 +1,19 @@
-import org.apache.kafka.clients.producer.ProducerInterceptor;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Map;
+import java.util.Properties;
 
-public class Example implements ProducerInterceptor<String, String> {
+public class ExampleProducer implements ProducerInterceptor<String, String> {
     @Override
     public ProducerRecord<String, String> onSend(ProducerRecord<String, String> record) {
-        return null;
+        System.out.println("onSend");
+        return record;
     }
 
     @Override
     public void onAcknowledgement(RecordMetadata metadata, Exception exception) {
-        System.out.println("ack step.");
+        System.out.println("onAcknowledgement");
     }
 
     @Override
@@ -23,5 +24,28 @@ public class Example implements ProducerInterceptor<String, String> {
     @Override
     public void configure(Map<String, ?> configs) {
 
+    }
+
+    KafkaProducer<String, String> producer;
+
+
+    public ExampleProducer() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        producer = new KafkaProducer<>(props);
+        String topic = "kafka-interceptors";
+
+        for (int i = 0; i < 1000000; i++) {
+            ProducerRecord<String, String> record = new ProducerRecord<>(topic, "key-" + i, "value-" + i);
+            producer.send(record);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new ExampleProducer();
     }
 }
